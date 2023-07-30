c09cc4b18e0f5a08f66696d2a49a169c8d417dcc
Mayank <mayankarora0877@gmail.com>
2023-07-30 18:02:39 +0530

    Developed Atlassian Dashboard

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navigation from './Navigation';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation />
+    <Main />
+    </div>
+  );
+}
+
+export default App;
